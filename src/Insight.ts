1c08e1e200bf0011bf461bcc9e33623353259826
bobpepers <bob.pepers@gmail.com>
2019-02-22 22:08:51 +0100

    correct insight url

--- src/Insight.ts
+++ src/Insight.ts
@@ -4,9 +4,9 @@ import { INetworkInfo } from "./Network"
 import { NetworkNames } from "./constants"
 
 const INSIGHT_BASEURLS: { [key: string]: string } = {
-  [NetworkNames.MAINNET]: "https://explorer.runebase.org/insight-api",
-  [NetworkNames.TESTNET]: "https://testnet.runebase.org/insight-api",
-  [NetworkNames.REGTEST]: "http://localhost:3001/insight-api",
+  [NetworkNames.MAINNET]: "https://explorer.runebase.io/runebase-insight-api",
+  [NetworkNames.TESTNET]: "https://testnet.runebase.io/runebase-insight-api",
+  [NetworkNames.REGTEST]: "http://localhost:3001/runebase-insight-api",
 }
 
 export class Insight {
